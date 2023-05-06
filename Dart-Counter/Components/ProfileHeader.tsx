d7aeaabdd33d39c781d1c48538a618f23c85dec6
TiboMessiaen <tibo.messiaen@student.howest.be>
2023-05-06 20:35:04 +0200

    feat: Go to profile when pres profile icon home

--- Dart-Counter/Components/ProfileHeader.tsx
+++ Dart-Counter/Components/ProfileHeader.tsx
@@ -3,12 +3,19 @@ import { Pressable, Text, View } from "react-native"
 import { colors } from "../Styles/colors"
 import { HomeStyle } from "../Styles/generic"
 
+
 import { Image } from "react-native"
+import { StackNavigationProp } from "@react-navigation/stack"
+import { useNavigation, ParamListBase } from '@react-navigation/native';
 
 export default ({profileName, nickname, image}:{profileName:string, nickname:string, image:string|null}) => {
+    const { navigate, setOptions, goBack } = useNavigation<StackNavigationProp<ParamListBase, 'HomeStack'>>()
+
     return (
         <View style={HomeStyle.profileHeader}>
-            <Image style={HomeStyle.profileIcon2} source={image == null ? require('../assets/images/ProfileIcon.png') : {uri:image}}/>
+            <Pressable onPress={() => navigate('Profile')}>
+                <Image style={HomeStyle.profileIcon2} source={image == null ? require('../assets/images/ProfileIcon.png') : {uri:image}}/>
+            </Pressable>
             <View style={HomeStyle.profileInfoContainer}>
                 <Text style={HomeStyle.profileName2}>{profileName}</Text>
                 <Text style={HomeStyle.profileText2}>{nickname}</Text>
